ee2bfb02d442e2982e8c9c6bec0634d8e8bd51dc
nicogig158@gmail.com <nicogig158@gmail.com@dab3cdf5-fdaf-9d89-8467-c6af71c98389>
2014-06-28 20:24:21 +0000

    Initial Commit

---
+++ trunk/app/src/main/java/nicolagigante/celieye/activity/Eye.java
@@ -0,0 +1,66 @@
+package nicolagigante.celieye.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.content.Intent;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import nicolagigante.celieye.R;
+
+
+public class Eye extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eye);
+        Animation fadeIn = new AlphaAnimation(0, 1);
+        fadeIn.setInterpolator(new AccelerateInterpolator()); //add this
+        fadeIn.setDuration(1200);
+        ImageButton b=(ImageButton)findViewById(R.id.imageButton);
+        AnimationSet animation = new AnimationSet(false); //change to false
+        animation.addAnimation(fadeIn);
+        b.startAnimation(animation);
+        Button settings=(Button) findViewById(R.id.button);
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickSettings();
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.eye, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            clickSettings();
+            return true; //Potrebbe essere eliminato(?)
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    private void clickSettings(){
+        Intent enabler=new Intent(this, Info.class);
+        startActivity(enabler);
+    }
+
+}
