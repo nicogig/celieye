ee71e9d54a5f3c5fc13d6f98b59178e3f1658454
eugenio.gigante@gmail.com <eugenio.gigante@gmail.com@dab3cdf5-fdaf-9d89-8467-c6af71c98389>
2014-08-02 09:20:07 +0000

    Animation added, modified layouts.

--- trunk/app/src/main/java/nicolagigante/celieye/dataBaseCache/DbKey.java
+++ trunk/app/src/main/java/nicolagigante/celieye/dataBaseCache/DbKey.java
@@ -4,27 +4,29 @@ package nicolagigante.celieye.dataBaseCache;
  * Created by Nico on 02/08/2014.
  */
 public class DbKey {
-    public static final String TABLE_CATEGORIES = "categories";
-    public static final String KEY_CATEGORIES_CATEGORY = "category";
-    public static final String KEY_CATEGORIES_NAME = "name";
-    public static final String KEY_CATEGORIES_ENGLISH = "english";
-    public static final String TABLE_COMPANIES = "companies";
-    public static final String KEY_COMPANIES_ID_COMPANY = "id_company";
-    public static final String KEY_COMPANIES_NAME = "name";
-    public static final String TABLE_BRANDS = "brands";
-    public static final String KEY_BRANDS_ID_BRAND = "id_brand";
-    public static final String KEY_BRANDS_ID_COMPANY = "id_company";
-    public static final String KEY_BRANDS_NAME = "name";
-    public static final String TABLE_PRODUCTS = "products";
-    public static final String KEY_PRODUCTS_ID_PRODUCT = "id_product";
-    public static final String KEY_PRODUCTS_ID_COMPANY = "id_company";
-    public static final String KEY_PRODUCTS_ID_CATEGORY = "id_category";
-    public static final String KEY_PRODUCTS_ID_BRAND = "id_brand";
-    public static final String KEY_PRODUCTS_TYPE = "type";
-    public static final String KEY_PRODUCTS_DESCRIPTION = "description";
-    public static final String TABLE_BARCODE = "barcode";
-    public static final String KEY_BARCODE_ID_PRODUCT = "id_product";
-    public static final String KEY_BARCODE_ID_BARCODE = "id_barcode";
-    public static final String KEY_BARCODE_FLAG_VALIDATE = "flag_validate";
+    public  final String TABLE_CATEGORIES = "categories";
+    public  final String KEY_CATEGORIES_CATEGORY = "category";
+    public  final String KEY_CATEGORIES_NAME = "name";
+    public  final String KEY_CATEGORIES_ENGLISH = "english";
+    public  final String TABLE_COMPANIES = "companies";
+    public  final String KEY_COMPANIES_ID_COMPANY = "id_company";
+    public  final String KEY_COMPANIES_NAME = "name";
+    public  final String TABLE_BRANDS = "brands";
+    public  final String KEY_BRANDS_ID_BRAND = "id_brand";
+    public  final String KEY_BRANDS_ID_COMPANY = "id_company";
+    public  final String KEY_BRANDS_NAME = "name";
+    public  final String TABLE_PRODUCTS = "products";
+    public  final String KEY_PRODUCTS_ID_PRODUCT = "id_product";
+    public  final String KEY_PRODUCTS_ID_COMPANY = "id_company";
+    public  final String KEY_PRODUCTS_ID_CATEGORY = "id_category";
+    public  final String KEY_PRODUCTS_ID_BRAND = "id_brand";
+    public  final String KEY_PRODUCTS_TYPE = "type";
+    public  final String KEY_PRODUCTS_DESCRIPTION = "description";
+    public  final String TABLE_BARCODE = "barcode";
+    public  final String KEY_BARCODE_ID_PRODUCT = "id_product";
+    public  final String KEY_BARCODE_ID_BARCODE = "id_barcode";
+    public  final String KEY_BARCODE_FLAG_VALIDATE = "flag_validate";
+    public DbKey (){
 
+    }
 }
