dee75e09805aa7625b43f833eae25bebe8324054
nicogig158@gmail.com <nicogig158@gmail.com@dab3cdf5-fdaf-9d89-8467-c6af71c98389>
2014-08-07 13:45:34 +0000

    Finished FTA, database now gets deleted from SDCard

--- trunk/app/src/main/java/nicolagigante/celieye/activity/BasicsTut.java
+++ trunk/app/src/main/java/nicolagigante/celieye/activity/BasicsTut.java
@@ -1,32 +1,25 @@
 package nicolagigante.celieye.activity;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import nicolagigante.celieye.R;
 
-public class First extends Activity {
-
-
+public class BasicsTut extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getActionBar().hide();
-        setContentView(R.layout.activity_first);
-
-
+        setContentView(R.layout.activity_basics_tut);
     }
 
-        public void first(View view){
-        Intent enabler=new Intent(this, BasicsTut.class);
+    public void stepnext(View view){
+        Intent enabler=new Intent(this, FinishTutorial.class);
         startActivity(enabler);
     }
 }
