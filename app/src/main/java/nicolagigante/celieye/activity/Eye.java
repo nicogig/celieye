c69bb87c485f0c707becbf0e8263027dc4fbdb72
nicogig158@gmail.com <nicogig158@gmail.com@dab3cdf5-fdaf-9d89-8467-c6af71c98389>
2014-08-10 15:16:54 +0000

    Major UI overhaul

--- app/src/main/java/nicolagigante/celieye/activity/Eye.java
+++ app/src/main/java/nicolagigante/celieye/activity/Eye.java
@@ -13,6 +13,7 @@ import android.view.animation.AccelerateInterpolator;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
+import android.widget.AutoCompleteTextView;
 import android.widget.ImageButton;
 
 import nicolagigante.celieye.R;
@@ -22,6 +23,7 @@ import nicolagigante.celieye.model.ApplicationProdotti;
 import nicolagigante.celieye.model.Prodotto;
 
 import android.view.View.OnClickListener;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -65,6 +67,16 @@ public class Eye extends Activity implements OnClickListener {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.eye, menu);
+        // Getting SearchView from XML layout by id defined there - my_search_view in this case
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        // Getting id for 'search_plate' - the id is part of generate R file,
+        // so we have to get id on runtime.
+        int searchPlateId = searchView.getContext().getResources().getIdentifier("android:id/search_plate", null, null);
+        // Getting the 'search_plate' LinearLayout.
+        View searchPlate = searchView.findViewById(searchPlateId);
+        // Setting background of 'search_plate' to earlier defined drawable.
+        searchPlate.setBackgroundResource(R.drawable.textfield_search);
+
         return true;
     }
 
@@ -82,14 +94,22 @@ public class Eye extends Activity implements OnClickListener {
             clickCart();
             return true;
         }
+        if (id == R.id.action_help) {
+            clickHelp();
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
     private void clickSettings(){
-        Intent enabler=new Intent(this, Settings.class);
+        Intent enabler=new Intent(this, AndroidFileDownloader.class);
         startActivity(enabler);
     }
     private void clickCart(){
         Intent enabler=new Intent(this, Cart.class);
+        startActivity(enabler);
+    }
+    private void clickHelp(){
+        Intent enabler = new Intent(this, Info.class);
         startActivity(enabler);
     }
    @Override
