e1e690dcd71a9fd6fac0bcea0a0e56d417da2257
eugenio.gigante@gmail.com <eugenio.gigante@gmail.com@dab3cdf5-fdaf-9d89-8467-c6af71c98389>
2014-08-12 06:29:05 +0000

    Finished FTA, database now gets deleted from SDCard

---
+++ trunk/app/src/main/java/nicolagigante/celieye/httpPost/AsyncHttpModule.java
@@ -0,0 +1,77 @@
+package nicolagigante.celieye.httpPost;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.IOException;
+
+/**
+ * Created by genio_2 on 12/08/2014.
+ */
+public class AsyncHttpModule extends AsyncTask<String, Void, Void> {
+
+    private final HttpClient Client = new DefaultHttpClient();
+    private String Content;
+    private String Error = null;
+   // private ProgressDialog Dialog = new ProgressDialog(AsyncronoustaskAndroidExample.this);
+
+    String uiUpdate ;
+
+    protected void onPreExecute() {
+        // NOTE: You can call UI Element here.
+
+        //UI Element
+        uiUpdate="Output : ";
+       // Dialog.setMessage("Downloading source..");
+        //Dialog.show();
+    }
+
+    // Call after onPreExecute method
+    protected Void doInBackground(String... urls) {
+        try {
+
+            // Call long running operations here (perform background computation)
+            // NOTE: Don't call UI Element here.
+
+            // Server url call by GET method
+            HttpGet httpget = new HttpGet(urls[0]);
+            ResponseHandler<String> responseHandler = new BasicResponseHandler();
+            Content = Client.execute(httpget, responseHandler);
+
+        } catch (ClientProtocolException e) {
+            Error = e.getMessage();
+            cancel(true);
+        } catch (IOException e) {
+            Error = e.getMessage();
+            cancel(true);
+        }
+
+        return null;
+    }
+
+    protected void onPostExecute(Void unused) {
+        // NOTE: You can call UI Element here.
+
+        // Close progress dialog
+       // Dialog.dismiss();
+
+        if (Error != null) {
+
+            uiUpdate="Output : "+Error;
+
+        } else {
+
+            uiUpdate="Output : "+Content;
+
+        }
+    }
+
+}
